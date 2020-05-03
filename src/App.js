b759631a2cf7b66894a4fd7bfcfaa6465bac5fdb
jnch009 <jngcheng@sfu.ca>
2020-05-02 20:25:59 -0700

    Responsive layout with skeleton

--- src/App.js
+++ src/App.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
 
 import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
+import Rank from './Components/Rank/Rank';
+import Particles from 'react-particles-js';
 
 import './App.css';
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 100,
+      density: {
+        enable: true,
+        area: 800
+      }
+    },
+  },
+  interactivity: {
+    events: {
+      onHover: {
+        enable: true,
+        mode: 'repulse',
+      },
+    },
+  },
+};
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <Particles className='particles' params={particleOptions} />
         <Navigation />
-        {/* <Logo />
+        <Logo />
+        <Rank />
         <ImageLinkForm />
-        <FaceRecognition /> */}
+        {/* <FaceRecognition /> */}
       </div>
     );
   }
