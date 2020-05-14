338701c3eac9ad3c3f071f9245af367fbcfeaf56
jnch009 <jngcheng@sfu.ca>
2020-05-14 04:02:30 -0700

    gh pages installed

--- jnch009smartbrain/build/service-worker.js
+++ jnch009smartbrain/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dcf3df46dc546b18de52a8befbf58ec8.js"
+  "/jnch009smartbrain/precache-manifest.892a04178c536c19f18b6022423dcf6c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jnch009smartbrain/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
