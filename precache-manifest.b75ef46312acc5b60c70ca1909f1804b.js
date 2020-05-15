a3d5a43442b8fe6985cff63bf059575d3b6fd468
jnch009 <jngcheng@sfu.ca>
2020-05-14 20:00:41 -0700

    Updates

---
+++ precache-manifest.b75ef46312acc5b60c70ca1909f1804b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "008ba13e3ada7a2ad548909bc73d1e7b",
+    "url": "/jnch009smartbrain/index.html"
+  },
+  {
+    "revision": "f40ceac416341bd8b72b",
+    "url": "/jnch009smartbrain/static/css/2.0d3435f4.chunk.css"
+  },
+  {
+    "revision": "5fae93a9993bb43fa2f8",
+    "url": "/jnch009smartbrain/static/css/main.e2661e1c.chunk.css"
+  },
+  {
+    "revision": "f40ceac416341bd8b72b",
+    "url": "/jnch009smartbrain/static/js/2.27438390.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/jnch009smartbrain/static/js/2.27438390.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5fae93a9993bb43fa2f8",
+    "url": "/jnch009smartbrain/static/js/main.556518be.chunk.js"
+  },
+  {
+    "revision": "568e2200d8422fc1bea1",
+    "url": "/jnch009smartbrain/static/js/runtime-main.ab5ecebb.js"
+  }
+]);
