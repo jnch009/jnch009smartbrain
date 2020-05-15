f043993fcc8bbbb86723692be679c01330c16867
jnch009 <jngcheng@sfu.ca>
2020-05-14 20:22:30 -0700

    flex column, fixing error persisting, hiding build

--- jnch009smartbrain/src/Components/Error/Error.js
+++ jnch009smartbrain/src/Components/Error/Error.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Error = props => {
   return (
-    <label className='br4 pa3 bg-red z-1 absolute dib center left-0 right-0 w-50 top-2'>
+    <label className='br4 pa3 bg-red z-1 absolute dib center flex-column left-0 right-0 w-50 top-2'>
       <h1 className='mv0'>ALERT!</h1>
       <br />
       {props.children}
