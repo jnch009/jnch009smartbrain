ac05957bd8392dcec5f5d619ecca4c9fc7be338b
jnch009 <jngcheng@sfu.ca>
2020-05-16 00:49:39 -0700

    logging headers cookies

--- jnch009smartbrainapi/server.js
+++ jnch009smartbrainapi/server.js
@@ -47,6 +47,7 @@ app.use(cookieParser());
 
 const verifyJWT = (req, res, next) => {
   console.log(req.cookies);
+  console.log(req.headers.cookies);
   jwt.verify(req.cookies.jwt, process.env.JWT_SECRET, (err, decoded) => {
     if (decoded) {
       req.user = decoded.user;
