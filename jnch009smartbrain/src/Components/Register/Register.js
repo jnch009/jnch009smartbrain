32ee9d12530632b38b8dbee68fa6705d2c567d94
jnch009 <jngcheng@sfu.ca>
2020-05-16 20:42:50 -0700

    register remove local storage

--- jnch009smartbrain/src/Components/Register/Register.js
+++ jnch009smartbrain/src/Components/Register/Register.js
@@ -67,13 +67,6 @@ class Register extends Component {
         .then(resp => resp.json())
         .then(data => {
           if (data?.id) {
-            localStorage.setItem(
-              'currentSession',
-              JSON.stringify({
-                data,
-                ...this.props.sessionExp(),
-              }),
-            );
             this.props.loadUser(data);
             this.props.onRouteChange('home');
           } else {
