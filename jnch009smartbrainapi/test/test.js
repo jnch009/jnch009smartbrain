efc4dcbb2750c2fa9177691ea1727fecd7bc56b6
jnch009 <jngcheng@sfu.ca>
2020-05-25 17:55:34 -0700

    finally managed to successfully add psql into my tests

--- jnch009smartbrainapi/test/test.js
+++ jnch009smartbrainapi/test/test.js
@@ -6,6 +6,7 @@ const bcrypt = require('bcrypt');
 const chai = require('chai');
 const sinon = require('sinon');
 const knex = require('knex');
+require('dotenv').config();
 
 const expect = chai.expect;
 
@@ -16,8 +17,15 @@ const db = knex({
     user: 'jnch009',
     password: process.env.DB_PASS,
     database: 'jnch009smartbrain',
+    pool: { min: 0 },
   },
 });
+const saltRounds = 10;
+const testUser = {
+  password: 'testing12345',
+  name: 'jeremy',
+  email: 'jeremy@gmail.com',
+};
 
 describe('Array', function () {
   describe('#indexOf()', function () {
@@ -28,23 +36,85 @@ describe('Array', function () {
 });
 
 describe('/signin', function () {
+  before(() => {
+    return new Promise(resolve => {
+      bcrypt.hash(testUser.password, saltRounds, function (err, hash) {
+        if (err) {
+          console.log(err);
+        }
+
+        db.transaction(trx => {
+          trx
+            .insert({
+              hash: hash,
+              email: testUser.email,
+            })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+              return trx('users')
+                .returning('*')
+                .insert({
+                  name: testUser.name,
+                  email: loginEmail[0],
+                  joined: new Date(),
+                })
+                .catch(() => res.status(400).json('User not registered'));
+            })
+            .then(trx.commit)
+            .catch(trx.rollback);
+        })
+          .catch(() => res.status(500).json(apiError))
+          .then(() => resolve());
+      });
+    });
+  });
+
   let req = { body: { email: '', password: '' } };
   let res = {
     cookie: sinon.spy(),
     json: sinon.spy(),
     status: function (resStatus) {
-      expect(resStatus).to.equal(400);
+      sinon.spy();
       return this;
     },
   };
 
   it('sign in missing fields', function () {
     signin.handleSignIn(req, res, db, bcrypt, apiError, jwt);
+    console.log(res);
   });
 
   it('sign in missing password', function () {
-      req.body.email = 'test@gmail.com';
-      req.body.password = 'test123';
-      signin.handleSignIn(req, res, db, bcrypt, apiError, jwt);
+    req.body.email = 'test@gmail.com';
+    req.body.password = 'test123';
+    signin.handleSignIn(req, res, db, bcrypt, apiError, jwt);
+  });
+
+  it('signed in successfully', function(){
+    req.body.email = testUser.email;
+    req.body.password = testUser.password;
+    signin.handleSignIn(req, res, db, bcrypt, apiError, jwt);
+  })
+
+  after(() => {
+    return new Promise((resolve, reject) =>
+      db
+        .transaction(trx => {
+          trx('login')
+            .where('email', testUser.email)
+            .del()
+            .then(() => {
+              return trx('users').where('email', testUser.email).del();
+            })
+            .then(trx.commit)
+            .catch(trx.rollback);
+        })
+        .then(() => {
+          db.destroy();
+          resolve();
+        })
+        .catch(err => reject(err)),
+    );
   });
 });
