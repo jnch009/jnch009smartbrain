be2b87024a0b5dfa1541678a0346c9bd5f4731ec
jnch009 <jngcheng@sfu.ca>
2020-05-26 00:26:18 -0700

    seeded test data

---
+++ jnch009smartbrainapi/db/seeds/test/smartbrain_users.js
@@ -0,0 +1,13 @@
+exports.seed = function (knex) {
+  // Deletes ALL existing entries
+  return knex('users')
+    .del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        { name: 'test1', email: 'test1@gmail.com' },
+        { name: 'test2', email: 'test2@gmail.com' },
+        { name: 'test3', email: 'test3@gmail.com' },
+      ]);
+    });
+};
