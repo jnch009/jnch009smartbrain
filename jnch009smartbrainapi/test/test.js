6120c4b9978095135143db997e18b7d475f4b451
jnch009 <jngcheng@sfu.ca>
2020-05-26 22:45:35 -0700

    root endpoint tests

--- jnch009smartbrainapi/test/test.js
+++ jnch009smartbrainapi/test/test.js
@@ -19,20 +19,6 @@ describe('Array', function () {
   });
 });
 
-describe('root', function () {
-  it('get root', function (done) {
-    chai
-      .request(app)
-      .get('/')
-      .end(function (err, res) {
-        expect(res).to.have.status(401);
-        expect(res.body).to.equal('Unauthorized, please log in');
-        done();
-      });
-  });
-});
-
-//nest describe blocks to have certain tests with and without auth
 describe('signin', function () {
   beforeEach(function (done) {
     knex.migrate.rollback().then(function () {
@@ -61,7 +47,7 @@ describe('signin', function () {
         done();
       });
   });
-  
+
   it('password missing', function (done) {
     chai
       .request(app)
@@ -73,8 +59,8 @@ describe('signin', function () {
         done();
       });
   });
-  
-  it('incorrect credentials', function(done){
+
+  it('incorrect credentials', function (done) {
     chai
       .request(app)
       .post('/signin')
@@ -88,7 +74,7 @@ describe('signin', function () {
         expect(res.body).to.equal('access denied');
         done();
       });
-  })
+  });
 
   it('signed in successfully', function (done) {
     chai
@@ -110,5 +96,52 @@ describe('signin', function () {
         res.body.score.should.equal(0);
         done();
       });
+  });
+});
+
+//nest describe blocks to have certain tests with and without auth
+describe('root', function () {
+  it('get root without auth', function (done) {
+    chai
+      .request(app)
+      .get('/')
+      .end(function (err, res) {
+        expect(res).to.have.status(401);
+        expect(res.body).to.equal('Unauthorized, please log in');
+        done();
+      });
+  });
+
+  describe('root route with auth', function () {
+    beforeEach(function (done) {
+      knex.migrate.rollback().then(function () {
+        knex.migrate.latest().then(function () {
+          return knex.seed.run().then(function () {
+            done();
+          });
+        });
+      });
+    });
+
+    afterEach(function (done) {
+      knex.migrate.rollback().then(function () {
+        done();
+      });
+    });
+    
+    it('get root with auth', function(done){
+      let agent = chai.request.agent(app);
+      agent.post('/signin').send({
+        email: 'test1@gmail.com',
+        password: process.env.TEST_PASS,
+      }).then(function(res){
+        expect(res).to.have.cookie('jwt')
+        return agent.get('/').then(function(res){
+          expect(res).to.have.status(200);
+          agent.close();
+          done();
+        })
+      });
+    })
   });
 });
