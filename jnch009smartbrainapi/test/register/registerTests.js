f79a37414ee391de855f117210b8aa8c4df05914
jnch009 <jngcheng@sfu.ca>
2020-05-27 22:15:40 -0700

    updating signin tests and finishing register tests

--- jnch009smartbrainapi/test/register/registerTests.js
+++ jnch009smartbrainapi/test/register/registerTests.js
@@ -8,8 +8,10 @@ const {
   expect,
 } = require('../index');
 
-module.exports = function SignInTests() {
-  describe('signin', function () {
+module.exports = function RegisterTests() {
+  const [email, password, name] = ['test4@gmail.com', 'testing12345', 'test4'];
+
+  describe('register', function () {
     beforeEach(function (done) {
       knex.migrate.rollback().then(function () {
         knex.migrate.latest().then(function () {
@@ -26,62 +28,88 @@ module.exports = function SignInTests() {
       });
     });
 
-    it('email missing', function (done) {
+    it('missing email', function (done) {
       chai
         .request(app)
-        .post('/signin')
-        .end(function (err, res) {
-          res.should.have.status(400);
-          res.should.be.json;
-          expect(res.body).to.equal('Cannot leave fields empty');
-          done();
-        });
-    });
-
-    it('password missing', function (done) {
-      chai
-        .request(app)
-        .post('/signin')
-        .end(function (err, res) {
-          res.should.have.status(400);
-          res.should.be.json;
-          expect(res.body).to.equal('Cannot leave fields empty');
-          done();
-        });
-    });
-
-    it('incorrect credentials', function (done) {
-      chai
-        .request(app)
-        .post('/signin')
+        .post('/register')
         .send({
-          email: 'test1@gmail.com',
-          password: 'yoyoyoma',
+          password: password,
+          name: name,
         })
         .end(function (err, res) {
-          res.should.have.status(401);
+          res.should.have.status(400);
           res.should.be.json;
-          expect(res.body).to.equal('access denied');
+          expect(res.body).to.equal('Cannot leave fields empty');
           done();
         });
     });
 
-    it('signed in successfully', function (done) {
+    it('missing password', function (done) {
       chai
         .request(app)
-        .post('/signin')
+        .post('/register')
         .send({
-          email: 'test1@gmail.com',
-          password: process.env.TEST_PASS,
+          email: email,
+          name: name,
+        })
+        .end(function (err, res) {
+          res.should.have.status(400);
+          res.should.be.json;
+          expect(res.body).to.equal('Cannot leave fields empty');
+          done();
+        });
+    });
+
+    it('missing name', function (done) {
+      chai
+        .request(app)
+        .post('/register')
+        .send({
+          email: email,
+          password: password,
+        })
+        .end(function (err, res) {
+          res.should.have.status(400);
+          res.should.be.json;
+          expect(res.body).to.equal('Cannot leave fields empty');
+          done();
+        });
+    });
+
+    it('duplicate email', function (done) {
+      chai
+        .request(app)
+        .post('/register')
+        .send({
+          email: 'test3@gmail.com',
+          name: name,
+          password: password,
+        })
+        .end(function (err, res) {
+          res.should.have.status(400);
+          res.should.be.json;
+          expect(res.body).to.equal('User not registered');
+          done();
+        });
+    });
+
+    it('successfully registered', function (done) {
+      chai
+        .request(app)
+        .post('/register')
+        .send({
+          email: email,
+          name: name,
+          password: password,
         })
         .end(function (err, res) {
           res.should.have.status(200);
           res.should.be.json;
           expect(res).to.have.cookie('jwt');
           res.body.should.have.property('name');
-          res.body.name.should.equal('test1');
+          res.body.name.should.equal(name);
           res.body.should.have.property('email');
-          res.body.email.should.equal('test1@gmail.com');
+          res.body.email.should.equal(email);
           res.body.should.have.property('score');
           res.body.score.should.equal(0);
           res.body.should.have.property('joined');
