149c438a351022ba466160f020d5ce7865661efc
jnch009 <jngcheng@sfu.ca>
2020-05-29 20:45:07 -0700

    tests for image endpoint

---
+++ jnch009smartbrainapi/test/image/imageTests.js
@@ -0,0 +1,137 @@
+const {
+  assert,
+  chai,
+  should,
+  chaiHttp,
+  knex,
+  app,
+  expect,
+} = require('../index');
+
+module.exports = function ImageTests() {
+  const mockInput = 'akjhdfkjsldjfalksdfasdfjklasdf';
+  const email = 'test3@gmail.com';
+  const agent = chai.request.agent(app);
+
+  describe('image', function () {
+    describe('clarifai API call', function () {
+      beforeEach(function (done) {
+        knex.migrate.rollback().then(function () {
+          knex.migrate.latest().then(function () {
+            return knex.seed.run().then(function () {
+              done();
+            });
+          });
+        });
+      });
+
+      afterEach(function (done) {
+        knex.migrate.rollback().then(function () {
+          done();
+        });
+      });
+
+      it('invalid input', function (done) {
+        agent
+          .post('/signin')
+          .send({
+            email: email,
+            password: process.env.TEST_PASS,
+          })
+          .then(function (res) {
+            expect(res).to.have.cookie('jwt');
+            return agent
+              .post('/imageURL')
+              .send({
+                input: mockInput,
+              })
+              .then(function (res) {
+                res.should.have.status(400);
+                res.should.be.json;
+                expect(res.body).to.equal(
+                  'Please review your input or use another image',
+                );
+              })
+              .catch(err => console.log(err.message))
+              .finally(() => {
+                done();
+              });
+          });
+      });
+    });
+
+    describe('profile score update', function () {
+      beforeEach(function (done) {
+        knex.migrate.rollback().then(function () {
+          knex.migrate.latest().then(function () {
+            return knex.seed.run().then(function () {
+              done();
+            });
+          });
+        });
+      });
+
+      afterEach(function (done) {
+        knex.migrate.rollback().then(function () {
+          done();
+        });
+      });
+
+      it('update score on invalid user', function (done) {
+        agent
+          .post('/signin')
+          .send({
+            email: email,
+            password: process.env.TEST_PASS,
+          })
+          .then(function (res) {
+            expect(res).to.have.cookie('jwt');
+            return agent
+              .put('/image')
+              .send({
+                id: 123,
+              })
+              .then(function (res) {
+                res.should.have.status(404);
+                res.should.be.json;
+                expect(res.body).to.equal(
+                  'Cannot increment score on invalid user',
+                );
+              })
+              .catch(err => console.log(err.message))
+              .finally(() => {
+                done();
+              });
+          });
+      });
+      
+      it('update score successfully', function (done) {
+        agent
+          .post('/signin')
+          .send({
+            email: email,
+            password: process.env.TEST_PASS,
+          })
+          .then(function (res) {
+            expect(res).to.have.cookie('jwt');
+            return agent
+              .put('/image')
+              .send({
+                id: 3,
+              })
+              .then(function (res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.have.property('score');
+                res.body.score.should.equal(1);
+              })
+              .catch(err => console.log(err.message))
+              .finally(() => {
+                done();
+                agent.close();
+              });
+          });
+      });
+    });
+  });
+};
