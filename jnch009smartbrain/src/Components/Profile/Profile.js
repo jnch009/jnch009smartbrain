b5297210f62b3307d5560d406607da476a042068
jnch009 <jngcheng@sfu.ca>
2020-05-31 00:21:43 -0700

    skeleton of profile page

--- jnch009smartbrain/src/Components/Profile/Profile.js
+++ jnch009smartbrain/src/Components/Profile/Profile.js
@@ -1,55 +1,45 @@
 import React from 'react';
 import './Profile.css';
 
+const article = (title, value) => (
+  <article
+    class='flex justify-around dt w-100 bb b--black-05 pa4 mt2'
+    href='#0'
+  >
+    <div class='dtc v-mid pl3'>
+      <h1 class='f4 f5-ns fw6 lh-title black mv0'>{title}</h1>
+      <h2 class='f4 fw4 mt0 mb0 black-60'>{value}</h2>
+    </div>
+  </article>
+);
+
 const Profile = () => {
   return (
     <>
-      <main class='mw6 center'>
-        <article class='dt w-100 bb b--black-05 pb2 mt2' href='#0'>
-          <div class='dtc w2 w3-ns v-mid'>
-            <img
-              src='http://mrmrs.github.io/photos/p/2.jpg'
-              class='ba b--black-10 db br-100 w2 w3-ns h2 h3-ns'
-            />
-          </div>
-          <div class='dtc v-mid pl3'>
-            <h1 class='f6 f5-ns fw6 lh-title black mv0'>Young Gatchell </h1>
-            <h2 class='f6 fw4 mt0 mb0 black-60'>@yg</h2>
-          </div>
-          <div class='dtc v-mid'>
-            <form class='w-100 tr'>
-              <button
-                class='f6 button-reset bg-white ba b--black-10 dim pointer pv1 black-60'
-                type='submit'
-              >
-                + Follow
-              </button>
-            </form>
-          </div>
-        </article>
-        <article class='dt w-100 bb b--black-05 pb2 mt2' href='#0'>
-          <div class='dtc w2 w3-ns v-mid'>
-            <img
-              src='http://mrmrs.github.io/photos/p/3.jpg'
-              class='ba b--black-10 db br-100 w2 w3-ns h2 h3-ns'
-            />
-          </div>
-          <div class='dtc v-mid pl3'>
-            <h1 class='f6 f5-ns fw6 lh-title black mv0'>Arnoldo Degraff</h1>
-            <h2 class='f6 fw4 mt0 mb0 black-60'>@99xx88randomhandle</h2>
-          </div>
-          <div class='dtc v-mid'>
-            <form class='w-100 tr'>
-              <button
-                class='f6 button-reset bg-white ba b--black-10 dim pointer pv1 black-60'
-                type='submit'
-              >
-                + Follow
-              </button>
-            </form>
-          </div>
-        </article>
+      <main class='mw6 center profileContainer'>
+        {article('Name: ', 'Jeremy Ng')}
+        {article('Email: ', 'jngchenghin@gmail.com')}
+        {article('Score: ', 0)}
+        {article('Joined: ', Date.now())}
       </main>
+      <a
+        class='f6 link dim br-pill ph3 pv2 ma3 dib white bg-hot-pink'
+        href='#0'
+      >
+        Update Information
+      </a>
+      <a
+        class='f6 link dim br-pill ph3 pv2 ma3 dib white bg-hot-pink'
+        href='#0'
+      >
+        Update Password
+      </a>
+      <a
+        class='f6 link dim br-pill ph3 pv2 ma3 dib white bg-hot-pink'
+        href='#0'
+      >
+        Delete Account
+      </a>
     </>
   );
 };
