004513558cb3dba48bff083d16ff4e6ed126382e
jnch009 <jngchenghin@gmail.com>
2020-06-06 17:29:08 -0700

    more descriptive error when register duplicated email

--- jnch009smartbrainapi/controllers/register.js
+++ jnch009smartbrainapi/controllers/register.js
@@ -2,7 +2,7 @@ const handleRegister = (req, res, db, bcrypt, saltRounds, apiError, jwt) => {
   const { email, name, password } = req.body;
 
   if (!email || !password || !name) {
-    return res.status(400).json('Cannot leave fields empty');
+    return res.status(400).json("Cannot leave fields empty");
   }
 
   bcrypt.hash(password, saltRounds, function (err, hash) {
@@ -10,40 +10,41 @@ const handleRegister = (req, res, db, bcrypt, saltRounds, apiError, jwt) => {
       console.log(err);
     }
 
-    db.transaction(trx => {
-      trx.insert({
+    db.transaction((trx) => {
+      trx
+        .insert({
           hash: hash,
           email: email,
         })
-        .into('login')
-        .returning('email')
-        .then(loginEmail => {
-          return trx('users')
-            .returning('*')
+        .into("login")
+        .returning("email")
+        .then((loginEmail) => {
+          return trx("users")
+            .returning("*")
             .insert({ name: name, email: loginEmail[0], joined: new Date() })
-            .then(user => {
+            .then((user) => {
               const token = jwt.sign(
                 { id: user[0].id },
                 process.env.JWT_SECRET,
                 {
-                  expiresIn: '1h',
-                },
+                  expiresIn: "1h",
+                }
               );
               res
-                .cookie('jwt', token, {
+                .cookie("jwt", token, {
                   httpOnly: true,
-                  sameSite: 'None',
-                  secure: process.env.NODE_ENV === 'PRODUCTION' ? true : false,
+                  sameSite: "None",
+                  secure: process.env.NODE_ENV === "PRODUCTION" ? true : false,
                   expires: new Date(Date.now() + 3.6e6),
                 })
                 .json(user[0]);
             })
-            .catch(() => res.status(400).json('User not registered'));
+            .catch(() => res.status(400).json("User not registered"));
         })
         .then(trx.commit)
         .catch(() => {
           trx.rollback;
-          res.status(400).json('User not registered');
+          res.status(400).json("Email already in use, please use another one");
         });
     }).catch(() => res.status(500).json(apiError));
   });
