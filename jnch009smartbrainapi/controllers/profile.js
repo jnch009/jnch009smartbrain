d375c45071676a38ceb8fbac985914d4176a686c
jnch009 <jngchenghin@gmail.com>
2020-06-08 19:44:31 -0700

    promise and async PUT methods

--- jnch009smartbrainapi/controllers/profile.js
+++ jnch009smartbrainapi/controllers/profile.js
@@ -25,39 +25,77 @@ const handlePutProfile = async (req, res, db) => {
   const { id } = req.params;
   const { email, name } = req.body;
 
-  const userInfo = await db('users').where({ id });
-  if (userInfo.length === 0) {
-    return res.status(404).json('User to update not found');
-  } else if (email === '' || name === '') {
-    return res.status(400).json('Cannot leave fields blank');
-  }
-
-  await db
-    .transaction(trx => {
-      trx('users')
-        .where({ id })
-        .returning('*')
-        .update({
-          email: email !== undefined ? email : undefined,
-          name: name !== undefined ? name : undefined,
-        })
-        .then(update => {
-          return update[0].email !== undefined
-            ? trx('login')
-                .where({ id })
-                .update({ email: update[0].email })
-                .then(() => {
-                  res.json(update[0]);
-                })
-            : res.status(400).json('Failed to update user');
-        })
-        .then(trx.commit)
-        .catch(trx.rollback);
+  //Promise way
+  db('users')
+    .where({ id })
+    .then(userInfo => {
+      if (userInfo.length === 0) {
+        res.status(404).json('User to update not found');
+        throw 'User to update not found';
+      } else if (email === '' || name === '') {
+        res.status(400).json('Cannot leave fields blank');
+        throw 'Cannot leave fields blank';
+      }
     })
-    .catch(() => res.status(500).json('DB Error'));
+    .then(() => {
+      db.transaction(trx => {
+        trx('users')
+          .where({ id })
+          .returning('*')
+          .update({
+            email: email !== undefined ? email : undefined,
+            name: name !== undefined ? name : undefined,
+          })
+          .then(update => {
+            return update[0].email !== undefined
+              ? trx('login')
+                  .where({ id })
+                  .update({ email: update[0].email })
+                  .then(() => {
+                    res.json(update[0]);
+                  })
+              : res.status(400).json('Failed to update user');
+          })
+          .then(trx.commit)
+          .catch(trx.rollback);
+      }).catch(() => res.status(500).json('DB Error'));
+    })
+    .catch(err => err);
+
+  //Async await way
+  // const userInfo = await db('users').where({ id });
+  // if (userInfo.length === 0) {
+  //   return res.status(404).json('User to update not found');
+  // } else if (email === '' || name === '') {
+  //   return res.status(400).json('Cannot leave fields blank');
+  // }
+
+  // await db
+  //   .transaction(trx => {
+  //     trx('users')
+  //       .where({ id })
+  //       .returning('*')
+  //       .update({
+  //         email: email !== undefined ? email : undefined,
+  //         name: name !== undefined ? name : undefined,
+  //       })
+  //       .then(update => {
+  //         return update[0].email !== undefined
+  //           ? trx('login')
+  //               .where({ id })
+  //               .update({ email: update[0].email })
+  //               .then(() => {
+  //                 res.json(update[0]);
+  //               })
+  //           : res.status(400).json('Failed to update user');
+  //       })
+  //       .then(trx.commit)
+  //       .catch(trx.rollback);
+  //   })
+  //   .catch(() => res.status(500).json('DB Error'));
 };
 
-const handlePutProfilePassword = async (req, res, db, bcrypt, saltRounds) => {
+const handlePutProfilePassword = (req, res, db, bcrypt, saltRounds) => {
   const { id } = req.params;
   const { password, currentPassword } = req.body;
 
@@ -65,23 +103,52 @@ const handlePutProfilePassword = async (req, res, db, bcrypt, saltRounds) => {
     res.status(400).json('Nothing to be updated');
   }
 
-  const currentUser = await db('login').where({ id });
-  if (currentUser.length === 0) {
-    res.status(404).json('User to update not found');
-  } else if (!bcrypt.compareSync(currentPassword, currentUser[0].hash)) {
-    res.status(400).json('Current password is incorrect');
-  } else {
-    let hashedPass = bcrypt.hashSync(password, saltRounds);
-    const updateUser = await db('login').where({ id }).returning('*').update({
-      hash: hashedPass,
-    });
+  //Promise way
+  db('login')
+    .where({ id })
+    .then(currentUser => {
+      if (currentUser.length === 0) {
+        res.status(404).json('User to update not found');
+        throw 'User to update not found';
+      } else if (!bcrypt.compareSync(currentPassword, currentUser[0].hash)) {
+        res.status(400).json('Current password is incorrect');
+        throw 'Current password is incorrect';
+      }
+    })
+    .then(() => {
+      let hashedPass = bcrypt.hashSync(password, saltRounds);
+      db('login')
+        .where({ id })
+        .returning('*')
+        .update({ hash: hashedPass })
+        .then(updateUser => {
+          if (updateUser.length > 0) {
+            res.json('Password Updated');
+          } else {
+            res.status(400).json('Password not updated');
+          }
+        });
+    })
+    .catch(err => err);
 
-    if (updateUser.length > 0) {
-      res.json('Password Updated');
-    } else {
-      res.status(400).json('Password not updated');
-    }
-  }
+  // Async Await way
+  // const currentUser = await db('login').where({ id });
+  // if (currentUser.length === 0) {
+  //   res.status(404).json('User to update not found');
+  // } else if (!bcrypt.compareSync(currentPassword, currentUser[0].hash)) {
+  //   res.status(400).json('Current password is incorrect');
+  // } else {
+  //   let hashedPass = bcrypt.hashSync(password, saltRounds);
+  //   const updateUser = await db('login').where({ id }).returning('*').update({
+  //     hash: hashedPass,
+  //   });
+
+  //   if (updateUser.length > 0) {
+  //     res.json('Password Updated');
+  //   } else {
+  //     res.status(400).json('Password not updated');
+  //   }
+  // }
 };
 
 const handleDeleteProfile = (req, res, db, apiError) => {
