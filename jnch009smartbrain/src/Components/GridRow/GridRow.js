ab45e6b5060622cff63ca9d50dd702ecc696b778
jnch009 <jngchenghin@gmail.com>
2020-06-20 18:51:10 -0700

    password input type

--- jnch009smartbrain/src/Components/GridRow/GridRow.js
+++ jnch009smartbrain/src/Components/GridRow/GridRow.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const GridRow = ({ title, value, editable, handleChange }) => (
+const GridRow = ({ title, value, editable, password, handleChange }) => (
   <article
     className='flex justify-around dt w-100 bb b--black-05 pa4 mt2'
     href='#0'
@@ -9,7 +9,7 @@ const GridRow = ({ title, value, editable, handleChange }) => (
       <h1 className='f4 f5-ns fw6 lh-title black mv0'>{title}</h1>
       {editable ? (
         <input
-          type='text'
+          type={password ? 'password' : 'text'}
           className='f4 fw4 mt0 mb0 black-60'
           value={value}
           onChange={e => handleChange(e.target.value)}
