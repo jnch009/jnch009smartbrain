d04baba1f040963814d0dd72c14655f8b28dbd5a
jnch009 <jngchenghin@gmail.com>
2020-06-21 10:09:24 +0000

    Deploying to gh-pages from  @ b27c3bf2215ef83850e4fd270f65ee63debae033 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ff134a941e834e9010d90e36665e1fd8.js"
+  "/jnch009smartbrain/precache-manifest.9910ed6d08620085c86d111458528306.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/jnch009smartbrain/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
