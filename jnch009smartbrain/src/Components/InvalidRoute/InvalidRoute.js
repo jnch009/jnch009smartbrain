ec1e20b1514de9c737be9af8673837772fb16979
jnch009 <jngchenghin@gmail.com>
2020-06-27 17:05:31 -0700

    my failed attempt at comedy

--- jnch009smartbrain/src/Components/InvalidRoute/InvalidRoute.js
+++ jnch009smartbrain/src/Components/InvalidRoute/InvalidRoute.js
@@ -8,7 +8,7 @@ const InvalidRoute = ({ history }) => {
   return (
     <>
       <h1>404</h1>
-      <h1>This page does not exist</h1>
+      <h1>Sorry guv'nor, but the princess is in another castle</h1>
       <p className='pointer dim' onClick={handleGoBack}>
         Click here to go back
       </p>
