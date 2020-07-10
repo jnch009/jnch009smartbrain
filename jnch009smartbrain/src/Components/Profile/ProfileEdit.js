ef46e723b42a0490e2e98430ac7bf9ca1a557030
Jeremy Ng <jngchenghin@gmail.com>
2020-07-09 18:39:34 -0700

    fixing build errors

--- jnch009smartbrain/src/Components/Profile/ProfileEdit.js
+++ jnch009smartbrain/src/Components/Profile/ProfileEdit.js
@@ -52,7 +52,7 @@ const ProfileEdit = ({
   return (
     <>
       <div onKeyPress={e => editProfileEnterPress(e)} tabIndex='0'>
-        <main class='mw6 center profileContainer'>
+        <main className='mw6 center profileContainer'>
           <GridRow
             title='Name: '
             value={name}
@@ -69,7 +69,7 @@ const ProfileEdit = ({
           <GridRow title='Joined: ' value={joined} />
         </main>
         <button
-          class='f6 link dim br-pill ph3 pv2 ma3 dib white bg-hot-pink'
+          className='f6 link dim br-pill ph3 pv2 ma3 dib white bg-hot-pink'
           onClick={() => {
             handleEditConfirmation(true);
             history.push('/Profile');
