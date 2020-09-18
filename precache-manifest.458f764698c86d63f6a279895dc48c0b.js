358c2ed4ab3a5d65ac3fe7a854a566519912512b
jnch009 <jngchenghin@gmail.com>
2020-09-18 01:40:53 +0000

    Deploying to gh-pages from  @ c06aab65a289e8b172209a5a3a47dab59a731f67 🚀

---
+++ precache-manifest.458f764698c86d63f6a279895dc48c0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cc810d16036c9df56a94401fa47a283",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0759aec0bad3d9af3d9b",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a1914a64ed6907a4e188",
+    "url": "/static/css/main.4eac52c0.chunk.css"
+  },
+  {
+    "revision": "0759aec0bad3d9af3d9b",
+    "url": "/static/js/2.41565e72.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.41565e72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1914a64ed6907a4e188",
+    "url": "/static/js/main.d9d4f671.chunk.js"
+  },
+  {
+    "revision": "df0b3e318af7e7c71fee",
+    "url": "/static/js/runtime-main.c94a377b.js"
+  }
+]);
