f9429cac35dddc02caacd52f9ee09caec71923fe
Jeremy Ng <jngchenghin@gmail.com>
2020-09-18 20:45:51 -0700

    ESLINT configuration update

--- jnch009smartbrainapi/controllers/signout.js
+++ jnch009smartbrainapi/controllers/signout.js
@@ -3,7 +3,7 @@ const handleSignOut = (req, res) => {
     .clearCookie('jwt', {
       httpOnly: true,
       sameSite: process.env.NODE_ENV === 'production' ? 'none' : false,
-      secure: process.env.NODE_ENV === 'production' ? true : false,
+      secure: process.env.NODE_ENV === 'production' ? true : false
     })
     .json('Successfully signed out');
 };
