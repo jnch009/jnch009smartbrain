604316a6ae1ea4e26702407ff49d9063b091124b
Jeremy Ng <jngchenghin@gmail.com>
2020-09-18 21:50:00 -0700

    Bug/mobile not responsive (#41)
    * responsiveness
    
    * removing width of form className

--- jnch009smartbrain/src/Components/ImageLinkForm/ImageLinkForm.js
+++ jnch009smartbrain/src/Components/ImageLinkForm/ImageLinkForm.js
@@ -9,15 +9,15 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit, inputField }) => {
         {'This Magic Brain will detect faces in your pictures. Give it a try!'}
       </p>
       <div className='center'>
-        <div className='form center pa4 br3 shadow-5'>
+        <div className='form w-100 w-75-ns center pv4 pa4-ns br3 shadow-5 dib-ns flex flex-column items-center'>
           <input
-            className='f4 pa2 w-70 center'
+            className='f4 pa2 w-70 center mb0-ns mb2'
             type='text'
             value={inputField}
             onChange={e => onInputChange(e)}
           />
           <button
-            className='tc w-30 o-90 glow f6 link ph3 pv2 dib white bg-light-purple'
+            className='tc w-30-ns w-70 o-90 glow f6 link ph3 pv2 dib white bg-light-purple'
             onClick={onButtonSubmit}
           >
             Detect
